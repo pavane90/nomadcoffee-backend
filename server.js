30438e6e10a88a7890613af585736093f63dd9c1
Takenotsuka <pavane90@gmail.com>
2024-02-13 20:58:48 +0900

    3.10

--- server.js
+++ server.js
@@ -1,2 +1,10 @@
-import { ApolloServer, gql } from "apollo-server";
-import { typeDefs, resolvers } from "./schema";
+import { ApolloServer } from "apollo-server";
+import schema from "./schema";
+
+const server = new ApolloServer({
+  schema,
+});
+
+server
+  .listen()
+  .then(() => console.log("Server is running on http://localhost:4000/"));
