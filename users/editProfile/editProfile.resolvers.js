378175f0974ebd144b090e9c51b0536c96d394ee
Takenotsuka <pavane90@gmail.com>
2024-02-19 16:42:07 +0900

    pix

--- users/editProfile/editProfile.resolvers.js
+++ users/editProfile/editProfile.resolvers.js
@@ -1,65 +1,66 @@
-import { createWriteStream } from "fs";
 import bcrypt from "bcrypt";
 import client from "../../client";
 import { protectedResolver } from "../users.utils";
-
-const resolverFn = async (
-  _,
-  {
-    username,
-    email,
-    name,
-    location,
-    avatarURL,
-    githubUsername,
-    password: newPassword,
-  },
-  { loggedInUser }
-) => {
-  let convertedAvatarUrl = null;
-  if (avatarURL) {
-    const { filename, createReadStream } = await avatarURL;
-    const newFilename = `${loggedInUser.id}-${Date.now()}-${filename}`;
-    const readStream = createReadStream();
-    const writeStream = createWriteStream(
-      process.cwd() + "/uploads/" + newFilename
-    );
-    readStream.pipe(writeStream);
-    convertedAvatarUrl = `http://localhost:4000/static/${newFilename}`;
-  }
-
-  let uglyPassword = null;
-  if (newPassword) {
-    uglyPassword = await bcrypt.hash(newPassword, 10);
-  }
-  const updatedUser = await client.user.update({
-    where: {
-      id: loggedInUser.id,
-    },
-    data: {
-      username,
-      email,
-      name,
-      location,
-      githubUsername,
-      ...(uglyPassword && { password: uglyPassword }),
-      ...(convertedAvatarUrl && { avatarURL: convertedAvatarUrl }),
-    },
-  });
-  if (updatedUser.id) {
-    return {
-      ok: true,
-    };
-  } else {
-    return {
-      ok: false,
-      error: "Could not update profile.",
-    };
-  }
-};
+import { createWriteStream } from "fs";
 
 export default {
   Mutation: {
-    editProfile: protectedResolver(resolverFn),
+    editProfile: protectedResolver(
+      async (
+        _,
+        {
+          username,
+          email,
+          name,
+          location,
+          password: newPassword,
+          avatarURL: avatar,
+          githubUsername,
+        },
+        { loggedInUser }
+      ) => {
+        let avatarURL = null;
+        if (avatar) {
+          const { filename, createReadStream } = await avatar;
+          const newFilename = `${loggedInUser.id}-${Date.now()}-${filename}`;
+          const readStream = createReadStream();
+          const writeStream = createWriteStream(
+            process.cwd() + "/uploads/" + newFilename
+          );
+          console.log(newFilename);
+          readStream.pipe(writeStream);
+          avatarURL = `http://localhost:4000/static/${newFilename}`;
+        }
+
+        let uglyPassword = null;
+        if (newPassword) {
+          uglyPassword = await bcrypt.hash(newPassword, 10);
+        }
+        const updatedUser = await client.user.update({
+          where: {
+            id: loggedInUser.id,
+          },
+          data: {
+            username,
+            email,
+            name,
+            location,
+            ...(uglyPassword && { password: uglyPassword }),
+            ...(avatarURL && { avatarURL }),
+            githubUsername,
+          },
+        });
+        if (updatedUser.id) {
+          return {
+            ok: true,
+          };
+        } else {
+          return {
+            ok: false,
+            error: "Could not update profile.",
+          };
+        }
+      }
+    ),
   },
 };
