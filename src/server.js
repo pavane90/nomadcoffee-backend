e73e4397ac3c6a687db93796274d7ce7382341c1
Takenotsuka <pavane90@gmail.com>
2024-02-22 22:30:07 +0900

    commit introspection

--- src/server.js
+++ src/server.js
@@ -10,6 +10,7 @@ const apollo = new ApolloServer({
   resolvers,
   typeDefs,
   playground: true,
+  introspection: true,
   context: async ({ req }) => {
     return {
       loggedInUser: await getUser(req.headers.token),
